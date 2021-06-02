7cc19be4386d60bd5a9a85e65662e2429098e0c7
Art <d.art.shishkin@gmail.com>
2021-06-02 16:43:31 +0300

    User Mappers (#2)

---
+++ user-management/user-cmd-api/src/main/java/net/shyshkin/study/cqrs/user/cmd/api/mappers/AccountMapper.java
@@ -0,0 +1,12 @@
+package net.shyshkin.study.cqrs.user.cmd.api.mappers;
+
+import net.shyshkin.study.cqrs.user.cmd.api.dto.AccountDto;
+import net.shyshkin.study.cqrs.user.core.models.Account;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface AccountMapper {
+
+    Account toModel(AccountDto dto);
+
+}
---
+++ user-management/user-cmd-api/src/main/java/net/shyshkin/study/cqrs/user/cmd/api/mappers/UserMapper.java
@@ -0,0 +1,19 @@
+package net.shyshkin.study.cqrs.user.cmd.api.mappers;
+
+import net.shyshkin.study.cqrs.user.cmd.api.dto.UserCreateDto;
+import net.shyshkin.study.cqrs.user.core.models.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.UUID;
+
+@Mapper(
+        uses = {AccountMapper.class},
+        imports = {UUID.class}
+)
+public interface UserMapper {
+
+    @Mapping(target = "id", expression = "java(UUID.randomUUID().toString())")
+    User toModel(UserCreateDto dto);
+
+}
