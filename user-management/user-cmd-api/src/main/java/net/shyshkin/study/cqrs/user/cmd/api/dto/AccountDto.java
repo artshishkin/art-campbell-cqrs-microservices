8633e66be34e7b901db0bc6710b73b57d9e48113
Art <d.art.shishkin@gmail.com>
2021-06-02 17:10:48 +0300

    Added validation for User DTO (#2)

--- user-management/user-cmd-api/src/main/java/net/shyshkin/study/cqrs/user/cmd/api/dto/AccountDto.java
+++ user-management/user-cmd-api/src/main/java/net/shyshkin/study/cqrs/user/cmd/api/dto/AccountDto.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import net.shyshkin.study.cqrs.user.core.models.Role;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -14,7 +17,20 @@ import java.util.List;
 @Builder
 public class AccountDto {
 
+    @Size(min = 3, max = 255, message = "username must have at least 3 and at most 255 characters")
     private String username;
+
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$",
+            message = "Password must contain at least one digit" +
+                    ", one lowercase Latin character" +
+                    ", one uppercase Latin character" +
+                    ", one special character like `! @ # & ( )`" +
+                    ", must have a length of at least 8 characters and a maximum of 20 characters."
+    )
     private String password;
+
+    @NotNull(message = "User role is mandatory")
+    @Size(min = 1, message = "User must have at least 1 Role")
     private List<Role> roles;
 }
--- user-management/user-cmd-api/src/main/java/net/shyshkin/study/cqrs/user/cmd/api/dto/UserCreateDto.java
+++ user-management/user-cmd-api/src/main/java/net/shyshkin/study/cqrs/user/cmd/api/dto/UserCreateDto.java
@@ -5,15 +5,28 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserCreateDto {
 
+    @NotEmpty(message = "firstname is mandatory")
     private String firstname;
+
+    @NotEmpty(message = "lastname is mandatory")
     private String lastname;
+
+    @Email(message = "please provide a valid email address")
     private String emailAddress;
+
+    @NotNull(message = "please provide account credentials")
+    @Valid
     private AccountDto account;
 
 }
