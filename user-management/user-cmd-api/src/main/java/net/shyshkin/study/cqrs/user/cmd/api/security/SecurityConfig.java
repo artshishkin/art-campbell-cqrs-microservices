b79bc7e71d479035bed070f137515714e70a4fb7
Art <d.art.shishkin@gmail.com>
2021-06-02 21:59:35 +0300

    Security config to permit any endpoints for anonymous users (temporarily) (#2)

--- user-management/user-cmd-api/src/main/java/net/shyshkin/study/cqrs/user/cmd/api/security/SecurityConfig.java
+++ user-management/user-cmd-api/src/main/java/net/shyshkin/study/cqrs/user/cmd/api/security/SecurityConfig.java
@@ -2,14 +2,27 @@ package net.shyshkin.study.cqrs.user.cmd.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public org.springframework.security.crypto.password.PasswordEncoder passwordEncoder(){
+    public org.springframework.security.crypto.password.PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
 }
