96735644eadc3acea75e2bfcd8ebce4fdc79d0ad
Art <d.art.shishkin@gmail.com>
2021-06-02 10:19:24 +0300

    10. Axon Configuration (closes #1)

--- user-management/user-cmd-api/src/main/java/net/shyshkin/study/cqrs/user/cmd/api/UserCmdApiApplication.java
+++ user-management/user-cmd-api/src/main/java/net/shyshkin/study/cqrs/user/cmd/api/UserCmdApiApplication.java
@@ -1,9 +1,12 @@
 package net.shyshkin.study.cqrs.user.cmd.api;
 
+import net.shyshkin.study.cqrs.user.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserCmdApiApplication {
 
     public static void main(String[] args) {
