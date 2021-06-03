956ea2887dcbadf6f872d9b345da50d4ffb1b213
Art <d.art.shishkin@gmail.com>
2021-06-03 11:32:58 +0300

    onUserUpdatedEvent Testcontainers test (#2)

--- user-management/user-query-api/src/test/java/net/shyshkin/study/cqrs/user/query/api/handlers/UserEventHandlerTest.java
+++ user-management/user-query-api/src/test/java/net/shyshkin/study/cqrs/user/query/api/handlers/UserEventHandlerTest.java
@@ -6,15 +6,19 @@ import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.cqrs.user.core.dto.AccountDto;
 import net.shyshkin.study.cqrs.user.core.dto.UserCreateDto;
 import net.shyshkin.study.cqrs.user.core.models.Role;
+import net.shyshkin.study.cqrs.user.core.models.User;
 import net.shyshkin.study.cqrs.user.query.api.commontest.AbstractDockerComposeTest;
 import net.shyshkin.study.cqrs.user.query.api.repositories.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
@@ -23,6 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 @Slf4j
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 class UserEventHandlerTest extends AbstractDockerComposeTest {
 
     private static final Faker FAKER = Faker.instance(new Locale("en-GB"));
@@ -34,6 +39,8 @@ class UserEventHandlerTest extends AbstractDockerComposeTest {
 
     @Autowired
     UserRepository repository;
+
+    static User existingUser = null;
 
     @BeforeEach
     void setUp() {
@@ -49,6 +56,7 @@ class UserEventHandlerTest extends AbstractDockerComposeTest {
     }
 
     @Test
+    @Order(10)
     void onUserRegisteredEvent() {
 
         //given
@@ -72,10 +80,59 @@ class UserEventHandlerTest extends AbstractDockerComposeTest {
                             .hasValueSatisfying(user -> assertThat(user)
                                     .hasFieldOrPropertyWithValue("id", userId)
                                     .isEqualToIgnoringGivenFields(newUser, "id", "account"))
-                            .hasValueSatisfying(user -> log.debug("Registered user: {}", user))
                             .hasValueSatisfying(user -> assertThat(user.getAccount())
                                     .isEqualToIgnoringGivenFields(newUser.getAccount(), "password")
                                     .satisfies(account -> assertThat(account.getPassword()).startsWith("{bcrypt}")))
+                            .hasValueSatisfying(user -> {
+                                log.debug("Registered user: {}", user);
+                                existingUser = user;
+                            })
+                    ;
+                });
+    }
+
+    @Test
+    @Order(20)
+    void onUserUpdatedEvent() {
+
+        //given
+        if (existingUser == null)
+            onUserRegisteredEvent();
+
+        var updatedUser = new UserCreateDto();
+        updatedUser.setAccount(new AccountDto());
+        updatedUser.getAccount().setRoles(new ArrayList<>());
+        var userId = existingUser.getId();
+        BeanUtils.copyProperties(existingUser, updatedUser, "id");
+        BeanUtils.copyProperties(existingUser.getAccount(), updatedUser.getAccount());
+
+        log.debug("user dto: {}", updatedUser);
+        updatedUser.setFirstname(updatedUser.getFirstname().toUpperCase());
+        updatedUser.getAccount().setUsername(updatedUser.getAccount().getUsername().toUpperCase());
+        updatedUser.getAccount().setPassword(generatePassword());
+
+        //when
+        RequestEntity<UserCreateDto> requestEntity = RequestEntity.put("/{id}", userId).body(updatedUser);
+        ResponseEntity<BaseResponse> responseEntity = userCmdApiRestTemplate
+                .exchange(requestEntity, BaseResponse.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        BaseResponse body = responseEntity.getBody();
+        assertThat(body.getMessage()).isEqualTo("User updated successfully");
+
+        await()
+                .timeout(2, TimeUnit.SECONDS)
+                .untilAsserted(() -> {
+
+                    assertThat(repository.findById(userId))
+                            .hasValueSatisfying(user -> assertThat(user)
+                                    .hasFieldOrPropertyWithValue("id", userId)
+                                    .isEqualToIgnoringGivenFields(updatedUser, "id", "account"))
+                            .hasValueSatisfying(user -> assertThat(user.getAccount())
+                                    .isEqualToIgnoringGivenFields(updatedUser.getAccount(), "password")
+                                    .satisfies(account -> assertThat(account.getPassword()).startsWith("{bcrypt}")))
+                            .hasValueSatisfying(user -> log.debug("Updated user: {}", user))
                     ;
                 });
     }
@@ -83,7 +140,7 @@ class UserEventHandlerTest extends AbstractDockerComposeTest {
     private UserCreateDto createNewUser() {
         var accountDto = AccountDto.builder()
                 .username(FAKER.name().username())
-                .password(FAKER.regexify("[a-z]{6}[1-9]{6}[A-Z]{6}[!@#&()]{2}"))
+                .password(generatePassword())
                 .roles(List.of(Role.READ_PRIVILEGE))
                 .build();
 
@@ -93,6 +150,10 @@ class UserEventHandlerTest extends AbstractDockerComposeTest {
                 .emailAddress(FAKER.bothify("????##@gmail.com"))
                 .account(accountDto)
                 .build();
+    }
+
+    private String generatePassword() {
+        return FAKER.regexify("[a-z]{6}[1-9]{6}[A-Z]{6}[!@#&()]{2}");
     }
 
     @Data
