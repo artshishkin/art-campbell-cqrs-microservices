6dac2f600ce6402603a856c2b08d72e2616baa69
Art <d.art.shishkin@gmail.com>
2021-06-03 10:12:04 +0300

    UserEventHandlerTest onUserRegisteredEvent Testcontainers test (#2)

---
+++ user-management/user-query-api/src/test/java/net/shyshkin/study/cqrs/user/query/api/handlers/UserEventHandlerTest.java
@@ -0,0 +1,116 @@
+package net.shyshkin.study.cqrs.user.query.api.handlers;
+
+import com.github.javafaker.Faker;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.cqrs.user.core.dto.AccountDto;
+import net.shyshkin.study.cqrs.user.core.dto.UserCreateDto;
+import net.shyshkin.study.cqrs.user.core.models.Role;
+import net.shyshkin.study.cqrs.user.query.api.commontest.AbstractDockerComposeTest;
+import net.shyshkin.study.cqrs.user.query.api.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+@Slf4j
+class UserEventHandlerTest extends AbstractDockerComposeTest {
+
+    private static final Faker FAKER = Faker.instance(new Locale("en-GB"));
+
+    @Autowired
+    RestTemplateBuilder restTemplateBuilder;
+
+    RestTemplate userCmdApiRestTemplate;
+
+    @Autowired
+    UserRepository repository;
+
+    @BeforeEach
+    void setUp() {
+
+        String host = composeContainer.getUserCmdApiHost();
+        Integer port = composeContainer.getUserCmdApiPort();
+        String rootUri = String.format("http://%s:%d/api/v1/users", host, port);
+        log.debug("User Command API Root URI: {}", rootUri);
+
+        userCmdApiRestTemplate = restTemplateBuilder
+                .rootUri(rootUri)
+                .build();
+    }
+
+    @Test
+    void onUserRegisteredEvent() {
+
+        //given
+        var newUser = createNewUser();
+
+        //when
+        var responseEntity = userCmdApiRestTemplate.postForEntity("/", newUser, RegisterUserResponse.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        RegisterUserResponse body = responseEntity.getBody();
+        assertThat(body.getMessage()).isEqualTo("User registered successfully");
+        String userId = body.getId();
+        assertThat(userId).isNotEmpty();
+
+        await()
+                .timeout(2, TimeUnit.SECONDS)
+                .untilAsserted(() -> {
+
+                    assertThat(repository.findById(userId))
+                            .hasValueSatisfying(user -> assertThat(user)
+                                    .hasFieldOrPropertyWithValue("id", userId)
+                                    .isEqualToIgnoringGivenFields(newUser, "id", "account"))
+                            .hasValueSatisfying(user -> log.debug("Registered user: {}", user))
+                            .hasValueSatisfying(user -> assertThat(user.getAccount())
+                                    .isEqualToIgnoringGivenFields(newUser.getAccount(), "password")
+                                    .satisfies(account -> assertThat(account.getPassword()).startsWith("{bcrypt}")))
+                    ;
+                });
+    }
+
+    private UserCreateDto createNewUser() {
+        var accountDto = AccountDto.builder()
+                .username(FAKER.name().username())
+                .password(FAKER.regexify("[a-z]{6}[1-9]{6}[A-Z]{6}[!@#&()]{2}"))
+                .roles(List.of(Role.READ_PRIVILEGE))
+                .build();
+
+        return UserCreateDto.builder()
+                .firstname(FAKER.name().firstName())
+                .lastname(FAKER.name().lastName())
+                .emailAddress(FAKER.bothify("????##@gmail.com"))
+                .account(accountDto)
+                .build();
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static class BaseResponse {
+        private String message;
+    }
+
+    @Getter
+    @ToString
+    private static class RegisterUserResponse extends BaseResponse {
+
+        private final String id;
+
+        public RegisterUserResponse(String id, String message) {
+            super(message);
+            this.id = id;
+        }
+    }
+}
