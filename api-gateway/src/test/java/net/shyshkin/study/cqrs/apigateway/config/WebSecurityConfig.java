f0aaf45c53feaac744a7ef9b4def912bf1a5128a
Art <d.art.shishkin@gmail.com>
2021-06-07 21:54:41 +0300

    31. Testing - Security Configuration (#4)

---
+++ api-gateway/src/test/java/net/shyshkin/study/cqrs/apigateway/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package net.shyshkin.study.cqrs.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange()
+                .anyExchange().permitAll();
+
+        return http.build();
+    }
+}
