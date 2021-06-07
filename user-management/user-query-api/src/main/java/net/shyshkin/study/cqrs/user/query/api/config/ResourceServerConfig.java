8035a0dd6b3ddbae39b56abe3b44478d19492dc9
Art <d.art.shishkin@gmail.com>
2021-06-07 14:44:47 +0300

    31. Testing API Gateway Routes - User Query API Dockerfile (#4)

---
+++ user-management/user-query-api/src/main/java/net/shyshkin/study/cqrs/user/query/api/config/ResourceServerConfig.java
@@ -0,0 +1,20 @@
+package net.shyshkin.study.cqrs.user.query.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/actuator/health").permitAll()
+                .anyRequest().authenticated();
+    }
+}
