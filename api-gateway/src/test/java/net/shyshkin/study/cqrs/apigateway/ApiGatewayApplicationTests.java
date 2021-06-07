42b5c049ae659de090483932ee7c099ddc2b2b11
Art <d.art.shishkin@gmail.com>
2021-06-07 13:12:27 +0300

    30. Create an API Gateway with Spring Cloud Gateway - Initial project (#4 Section 5: API Gateway)

---
+++ api-gateway/src/test/java/net/shyshkin/study/cqrs/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.cqrs.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
