107ccb335d1b6f33afe8cb1dc0ebd7c3102dcbf8
Art <d.art.shishkin@gmail.com>
2021-06-08 11:53:36 +0300

    34. Bank Account Project Setup - Exclude AutoConfiguration for Datasource from Command API (#11)

--- bank-account/bankaccount-cmd-api/src/main/java/net/shyshkin/study/cqrs/bankaccount/cmd/api/BankAccountCmdApiApplication.java
+++ bank-account/bankaccount-cmd-api/src/main/java/net/shyshkin/study/cqrs/bankaccount/cmd/api/BankAccountCmdApiApplication.java
@@ -3,9 +3,10 @@ package net.shyshkin.study.cqrs.bankaccount.cmd.api;
 import net.shyshkin.study.cqrs.bankaccount.core.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @Import({AxonConfig.class})
 public class BankAccountCmdApiApplication {
 
