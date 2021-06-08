882a8b5ccb402624b76a6134e1abbcdee7cd3743
Art <d.art.shishkin@gmail.com>
2021-06-08 11:46:17 +0300

    34. Bank Account Project Setup - Security Config (#11)

---
+++ bank-account/bankaccount-cmd-api/src/main/java/net/shyshkin/study/cqrs/bankaccount/cmd/api/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package net.shyshkin.study.cqrs.bankaccount.cmd.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+}
