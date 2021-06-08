64557ae5d906f52ff0206761b41dd1f90aa6bc43
Art <d.art.shishkin@gmail.com>
2021-06-08 20:04:06 +0300

    OpenAccountCommand validation (#11)

--- bank-account/bankaccount-cmd-api/src/main/java/net/shyshkin/study/cqrs/bankaccount/cmd/api/commands/OpenAccountCommand.java
+++ bank-account/bankaccount-cmd-api/src/main/java/net/shyshkin/study/cqrs/bankaccount/cmd/api/commands/OpenAccountCommand.java
@@ -7,6 +7,10 @@ import lombok.NoArgsConstructor;
 import net.shyshkin.study.cqrs.bankaccount.core.models.AccountType;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.util.UUID;
 
@@ -19,10 +23,15 @@ public class OpenAccountCommand {
     @TargetAggregateIdentifier
     private UUID id;
 
+    @NotEmpty(message = "Account Holder Id is MANDATORY")
+    @Size(min = 36, max = 36, message = "Must be UUID and have 36 characters long")
     private String accountHolderId;
 
+    @NotNull(message = "Account type is MANDATORY")
     private AccountType accountType;
 
+    @NotNull(message = "Opening balance must not be null or empty")
+    @PositiveOrZero(message = "Opening balance must not be negative")
     private BigDecimal openingBalance;
 
 }
