cc73e2ad85d3040f805825c2ada54770b202f26b
Art <d.art.shishkin@gmail.com>
2021-06-09 15:00:49 +0300

    Test API Gateway for Withdrawing Funds (#11)

--- api-gateway/src/test/java/net/shyshkin/study/cqrs/apigateway/ApiGatewayApplicationAccountsTest.java
+++ api-gateway/src/test/java/net/shyshkin/study/cqrs/apigateway/ApiGatewayApplicationAccountsTest.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.cqrs.apigateway.commontest.AbstractDockerComposeTest;
 import net.shyshkin.study.cqrs.apigateway.dto.accounts.*;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -58,19 +61,23 @@ class ApiGatewayApplicationAccountsTest extends AbstractDockerComposeTest {
                 );
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "deposits,Funds deposited successfully",
+            "withdrawals,Funds withdrawn successfully",
+    })
     @Order(110)
-    void depositFunds_ok() {
+    void depositOrWithdrawFunds_ok(String endpoint, String expectedMessage) {
 
         //given
         UUID id = existingAccountId;
         var command = FundsCommand.builder()
                 .id(id)
-                .amount(new BigDecimal("100.00"))
+                .amount(new BigDecimal("10.00"))
                 .build();
 
         //when
-        webTestClient.put().uri("/api/v1/accounts/{id}/deposits", id)
+        webTestClient.put().uri("/api/v1/accounts/{id}/{endpoint}", id, endpoint)
                 .headers(headers -> headers.setBearerAuth(jwtAccessToken))
                 .bodyValue(command)
                 .exchange()
@@ -81,25 +88,27 @@ class ApiGatewayApplicationAccountsTest extends AbstractDockerComposeTest {
                 .value(baseResponse -> assertThat(baseResponse)
                         .satisfies(body -> log.debug("Response body: {}", body))
                         .hasNoNullFieldsOrProperties()
-                        .hasFieldOrPropertyWithValue("message", "Funds deposited successfully")
+                        .hasFieldOrPropertyWithValue("message", expectedMessage)
                 );
     }
 
-    @Test
+    @DisplayName("When depositing or withdrawing funds Id in URL and in Command should match")
+    @ParameterizedTest(name = "[{index} {arguments}]")
+    @ValueSource(strings = {"deposits", "withdrawals"})
     @Order(120)
-    void depositFunds_wrongId() {
+    void depositOrWithdrawFunds_wrongId(String endpoint) {
 
         //given
         UUID id = existingAccountId;
         var command = FundsCommand.builder()
                 .id(UUID.randomUUID())
-                .amount(new BigDecimal("100.00"))
+                .amount(new BigDecimal("10.00"))
                 .build();
 
         String expectedMessage = String.format("Account Id in URL `%s` does not match Id in command `%s`", id, command.getId());
 
         //when
-        webTestClient.put().uri("/api/v1/accounts/{id}/deposits", id)
+        webTestClient.put().uri("/api/v1/accounts/{id}/{endpoint}", id, endpoint)
                 .headers(headers -> headers.setBearerAuth(jwtAccessToken))
                 .bodyValue(command)
                 .exchange()
@@ -113,5 +122,38 @@ class ApiGatewayApplicationAccountsTest extends AbstractDockerComposeTest {
                         .hasFieldOrPropertyWithValue("message", expectedMessage)
                 );
     }
+
+    @DisplayName("When depositing or withdrawing funds Id in URL and in Command should match")
+    @ParameterizedTest(name = "[{index} {arguments}]")
+    @ValueSource(strings = {"deposits", "withdrawals"})
+    @Order(125)
+    void depositOrWithdrawFunds_validationFailed(String endpoint) {
+
+        //given
+        UUID id = existingAccountId;
+        var command = FundsCommand.builder()
+                .id(id)
+                .amount(new BigDecimal("-10.00"))
+                .build();
+
+        //when
+        webTestClient.put().uri("/api/v1/accounts/{id}/{endpoint}", id, endpoint)
+                .headers(headers -> headers.setBearerAuth(jwtAccessToken))
+                .bodyValue(command)
+                .exchange()
+
+                //then
+                .expectStatus().isBadRequest()
+                .expectBody(BaseResponse.class)
+                .value(baseResponse -> assertThat(baseResponse)
+                        .satisfies(body -> log.debug("Response body: {}", body))
+                        .hasNoNullFieldsOrProperties()
+                        .satisfies(body -> assertThat(body.getMessage())
+                                .contains("Validation failed for argument ")
+                                .contains("Amount must be positive")
+                        )
+                );
+    }
+
 
 }
