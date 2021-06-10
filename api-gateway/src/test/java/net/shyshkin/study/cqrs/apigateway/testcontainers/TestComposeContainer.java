8068e8c98dee70bebba9e2de403da5ed6749a575
Art <d.art.shishkin@gmail.com>
2021-06-10 12:49:11 +0300

    simplifying TestComposeContainer class to not expose unneeded ports (#11)

--- api-gateway/src/test/java/net/shyshkin/study/cqrs/apigateway/testcontainers/TestComposeContainer.java
+++ api-gateway/src/test/java/net/shyshkin/study/cqrs/apigateway/testcontainers/TestComposeContainer.java
@@ -15,17 +15,9 @@ public class TestComposeContainer extends DockerComposeContainer<TestComposeCont
     private static TestComposeContainer container;
 
     private static boolean containerStarted = false;
-    private String userCmdApiHost;
-    private Integer userCmdApiPort;
-
-    private String userQueryApiHost;
-    private Integer userQueryApiPort;
 
     private String oauthHost;
     private Integer oauthPort;
-
-    private String bankAccountCmdApiHost;
-    private Integer bankAccountCmdApiPort;
 
     public TestComposeContainer() {
         super(new File(COMPOSE_FILE_PATH));
@@ -34,9 +26,9 @@ public class TestComposeContainer extends DockerComposeContainer<TestComposeCont
     public static TestComposeContainer getInstance() {
         if (container == null) {
             container = new TestComposeContainer()
-                    .withExposedService("axon-server_1", 8124,
+                    .waitingFor("axon-server_1",
                             Wait.forLogMessage(".*Started AxonServer in.*\\n", 1))
-                    .withExposedService("mongo_1", 27017, Wait.forHealthcheck())
+                    .waitingFor("mongo_1", Wait.forHealthcheck())
                     .withExposedService("user-cmd-api_1", 8080, Wait.forHealthcheck())
                     .withExposedService("user-query-api_1", 8080, Wait.forHealthcheck())
                     .withExposedService("oauth20-server_1", 8080, Wait.forHealthcheck())
@@ -55,44 +47,14 @@ public class TestComposeContainer extends DockerComposeContainer<TestComposeCont
 
         containerStarted = true;
 
-        String axonHost = container.getServiceHost("axon-server_1", 8124);
-        Integer axonPort = container.getServicePort("axon-server_1", 8124);
-        String servers = axonHost + ":" + axonPort;
-        System.setProperty("AXON_SERVERS", servers);
-
-        log.debug("AXON_SERVERS: {}", servers);
-
-        String mongodbHost = container.getServiceHost("mongo_1", 27017);
-        Integer mongodbPort = container.getServicePort("mongo_1", 27017);
-
-        System.setProperty("MONGODB_HOST", mongodbHost);
-        System.setProperty("MONGODB_PORT", String.valueOf(mongodbPort));
-
-        log.debug("MongoDB: {}:{}", mongodbHost, mongodbPort);
-
-        userCmdApiHost = container.getServiceHost("user-cmd-api_1", 8080);
-        userCmdApiPort = container.getServicePort("user-cmd-api_1", 8080);
-
-        log.debug("user-cmd-api: {}:{}", userCmdApiHost, userCmdApiPort);
-        System.setProperty("USER_CMD_API_URI", String.format("http://%s:%d", userCmdApiHost, userCmdApiPort));
-
-        userQueryApiHost = container.getServiceHost("user-query-api_1", 8080);
-        userQueryApiPort = container.getServicePort("user-query-api_1", 8080);
-
-        log.debug("user-query-api: {}:{}", userQueryApiHost, userQueryApiPort);
-        System.setProperty("USER_QUERY_API_URI", String.format("http://%s:%d", userQueryApiHost, userQueryApiPort));
-
         oauthHost = container.getServiceHost("oauth20-server_1", 8080);
         oauthPort = container.getServicePort("oauth20-server_1", 8080);
 
         log.debug("oauth20-server: {}:{}", oauthHost, oauthPort);
 
-        bankAccountCmdApiHost = container.getServiceHost("bankaccount-cmd-api_1", 8080);
-        bankAccountCmdApiPort = container.getServicePort("bankaccount-cmd-api_1", 8080);
-
-        log.debug("bankaccount-cmd-api: {}:{}", bankAccountCmdApiHost, bankAccountCmdApiPort);
-        System.setProperty("BANKACCOUNT_CMD_API_URI", String.format("http://%s:%d", bankAccountCmdApiHost, bankAccountCmdApiPort));
-
+        setServiceUriSystemProperty("user-cmd-api", 8080);
+        setServiceUriSystemProperty("user-query-api", 8080);
+        setServiceUriSystemProperty("bankaccount-cmd-api", 8080);
         setServiceUriSystemProperty("bankaccount-query-api", 8080);
     }
 
