bdf6d6ae71414f48c12ef7cf335ca63bd3d89289
Art <d.art.shishkin@gmail.com>
2021-06-30 15:14:58 +0300

    15.9 Testing User Update Manually (#15)

--- user-management/user-cmd-api/src/test/java/net/shyshkin/study/cqrs/user/cmd/api/controllers/UpdateUserControllerManualTest.java
+++ user-management/user-cmd-api/src/test/java/net/shyshkin/study/cqrs/user/cmd/api/controllers/UpdateUserControllerManualTest.java
@@ -16,9 +16,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Collections;
 import java.util.List;
@@ -29,7 +31,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Disabled("Only for manual tests -> Start `docker-compose` first")
+@Disabled("Only for manual tests -> Start `infrastructure` and `auth-server` docker-compose files first")
+@ActiveProfiles("local")
 class UpdateUserControllerManualTest {
 
     private static final Faker FAKER = Faker.instance(new Locale("en-GB"));
@@ -43,6 +46,9 @@ class UpdateUserControllerManualTest {
     @Autowired
     UserMapper mapper;
 
+    // go to postman and get token for existing user, i.e. username = `shyshkin.art`, password = `P@ssW0rd!`
+    String jwtAccessToken = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJpemlDNVhVY1h0X1pSQmdhT0p6OFRSVGpEYm56bGZaTlh3V254UmNNbEkwIn0.eyJleHAiOjE2MjUwNTU0OTQsImlhdCI6MTYyNTA1NTE5NCwianRpIjoiZTJhYjU4MjYtOWM3Mi00OWFjLWI4ZGItOTQ4MWRkYmQ3MzdmIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL2thdGFyaW5hemFydCIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJlOWQ0YTVlZS00ZDgyLTQ4MDctOGMyMi02MWQ4MGZiNGYyODEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJzcHJpbmdiYW5rQ2xpZW50Iiwic2Vzc2lvbl9zdGF0ZSI6IjM3YmQ1NzU3LTNmNGQtNGU1My05NjcwLTcxNjI5MWU3ZmJjMiIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGVmYXVsdC1yb2xlcy1rYXRhcmluYXphcnQiLCJSRUFEX1BSSVZJTEVHRSIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iLCJXUklURV9QUklWSUxFR0UiXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiQXJ0IFNoeXNoa2luIiwicHJlZmVycmVkX3VzZXJuYW1lIjoic2h5c2hraW4uYXJ0IiwiZ2l2ZW5fbmFtZSI6IkFydCIsImZhbWlseV9uYW1lIjoiU2h5c2hraW4iLCJlbWFpbCI6ImQuYXJ0LnNoaXNoa2luQGdtYWlsLmNvbSJ9.pVBG_UkbXm8f5xA5wqRPWwD5FX30BTtzFEYrjb_CgpieVHcFZMWW92GQLNXCCknl6UA1jzvddZOSl-ihww2asXoHT27nhKVMtwFk_6tWYSWRKgVY4nYoAuJSt6DJ2LWqUZQBO96lK5jkxjYrWu7JStXGM41J1iTItrM0ai1SBoW0fqEHLcp-silp2lOayh4kG5uwu-tKXgSGWHD3ArCxita7VNFd1Nqk0g2OZ83xUi10DqbRiTRDg5usZ_kVb3MvhQyRtA-OmUmhmX35dNAd3GXL8UPRbt2QcF0jI7Qb8lRi44o9Bkru32kx-pJrIwe8tQ3fn2U9jgK2kuAO8pFoiQ";
+
     static User existingUser = null;
 
     @Test
@@ -54,7 +60,10 @@ class UpdateUserControllerManualTest {
         UserCreateDto dto = mapper.toDto(user);
 
         //when
-        RequestEntity<UserCreateDto> requestEntity = RequestEntity.put("/api/v1/users/{id}", userId).body(dto);
+        RequestEntity<UserCreateDto> requestEntity = RequestEntity
+                .put("/api/v1/users/{id}", userId)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtAccessToken)
+                .body(dto);
         ResponseEntity<BaseResponse> responseEntity = restTemplate
                 .exchange(requestEntity, BaseResponse.class);
 
@@ -79,7 +88,9 @@ class UpdateUserControllerManualTest {
             dto.setFirstname(null);
 
             //when
-            RequestEntity<UserCreateDto> requestEntity = RequestEntity.put("/api/v1/users/{id}", userId).body(dto);
+            RequestEntity<UserCreateDto> requestEntity = RequestEntity.put("/api/v1/users/{id}", userId)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtAccessToken)
+                    .body(dto);
             ResponseEntity<BaseResponse> responseEntity = restTemplate
                     .exchange(requestEntity, BaseResponse.class);
 
@@ -103,7 +114,10 @@ class UpdateUserControllerManualTest {
             dto.getAccount().setPassword("FOO");
 
             //when
-            RequestEntity<UserCreateDto> requestEntity = RequestEntity.put("/api/v1/users/{id}", userId).body(dto);
+            RequestEntity<UserCreateDto> requestEntity = RequestEntity
+                    .put("/api/v1/users/{id}", userId)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtAccessToken)
+                    .body(dto);
             ResponseEntity<BaseResponse> responseEntity = restTemplate
                     .exchange(requestEntity, BaseResponse.class);
 
@@ -127,7 +141,10 @@ class UpdateUserControllerManualTest {
             dto.getAccount().setRoles(null);
 
             //when
-            RequestEntity<UserCreateDto> requestEntity = RequestEntity.put("/api/v1/users/{id}", userId).body(dto);
+            var requestEntity = RequestEntity
+                    .put("/api/v1/users/{id}", userId)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtAccessToken)
+                    .body(dto);
             ResponseEntity<BaseResponse> responseEntity = restTemplate
                     .exchange(requestEntity, BaseResponse.class);
 
@@ -151,7 +168,10 @@ class UpdateUserControllerManualTest {
             dto.getAccount().setRoles(Collections.emptyList());
 
             //when
-            RequestEntity<UserCreateDto> requestEntity = RequestEntity.put("/api/v1/users/{id}", userId).body(dto);
+            var requestEntity = RequestEntity
+                    .put("/api/v1/users/{id}", userId)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtAccessToken)
+                    .body(dto);
             ResponseEntity<BaseResponse> responseEntity = restTemplate
                     .exchange(requestEntity, BaseResponse.class);
 
